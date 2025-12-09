d750289c6530f8dc27492645e15a1f14122357b2
DucThinhmta <nguyenducthinh28092006@gmail.com>
2025-12-09 22:45:29 +0700

    Update FoodProduct.cpp

--- FoodProduct.cpp
+++ FoodProduct.cpp
@@ -1,1 +1,15 @@
+include <iostream>
+#include "FoodProduct.h"
+using namespace std;
+string FoodProduct::type() const {
+    return "Food";
+}
+
+double FoodProduct::price_discount(int qty) const {
+    FoodProduct p;
+    if (p.price>=500) return price * qty*0.9;
+    if (p.price>=300&&p.price <500) return price * qty * 0.94;
+    return price * qty;
+}
+
 
