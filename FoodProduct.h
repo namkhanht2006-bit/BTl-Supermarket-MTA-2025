1491154a2334c7c4650466f4d0a1940efc5eace7
DucThinhmta <nguyenducthinh28092006@gmail.com>
2025-12-09 22:31:54 +0700

    Add FoodProduct class with file I/O and pricing methods

--- FoodProduct.h
+++ FoodProduct.h
@@ -1,1 +1,11 @@
+class FoodProduct:public product {
+    string expiry;
+    string nutrition;
+    public:
+    void readfile(istream &is);
+    void writefile(ostream &os);
+    void inputfile(istream &is);
+    double price_discount(int qty) const;
+    string type() const;
 
+};
