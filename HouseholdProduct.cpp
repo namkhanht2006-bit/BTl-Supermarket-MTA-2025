e69d8397311116040644e8f454cb3f08f96307e1
DucThinhmta <nguyenducthinh28092006@gmail.com>
2025-12-09 22:52:38 +0700

    Implement price discount logic in HouseholdProduct

--- HouseholdProduct.cpp
+++ HouseholdProduct.cpp
@@ -1,1 +1,14 @@
+include <iostream>
+#include "HouseholdProduct.h"
+using namespace std;
+string HouseholdProduct::type() const {
+    return "Household";
+}
+
+double HouseholdProduct::price_discount(int qty) const {
+    FoodProduct p;
+    if (p.price>=500) return price * qty*0.85;
+    if (p.price>=200&&p.price <500) return price * qty * 0.90;
+    return price * qty;
+}
 
