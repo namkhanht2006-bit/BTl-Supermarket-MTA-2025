10245589391ac9e75df8f29d279b2df289e55210
DucThinhmta <nguyenducthinh28092006@gmail.com>
2025-12-09 22:34:54 +0700

    Update FoodProduct.h

--- FoodProduct.h
+++ FoodProduct.h
@@ -1,10 +1,16 @@
 class FoodProduct:public product {
-    string expiry;
-    string nutrition;
+    string Expiry;
+    string Nutrition;
     public:
-    void readfile(istream &is);
-    void writefile(ostream &os);
-    void inputfile(istream &is);
+    void setId(const string &v){id =v; };
+    void setName(const std::string& v) { name = v; }
+    void setPrice(double v) { price = v; }
+    void setQuantity(int v) { quantity = v; }
+    void setBrand(const string& v) { brand = v; }
+    void setWeight(float v) { weight = v; }
+    void setWarranty(const string& v) { warranty = v; }
+    void setSeri(const string& v) { seri = v; }
+    
     double price_discount(int qty) const;
     string type() const;
 
