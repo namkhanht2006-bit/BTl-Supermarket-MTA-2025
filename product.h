59a4f78af3c51efdfb787dcda3e783e77a2d9c24
namkhanht2006-bit <namkhanht2006@gmail.com>
2025-12-09 12:30:20 +0700

    Update product.h

--- product.h
+++ product.h
@@ -1,7 +1,163 @@
-using namespace std
-class Product{
+#include<string>
+#include<iostream>
+#include<vector>
+#include<memory>
+#include<optional>
+using namespace std;
+class product {
+protected:
+    string name;
+    string id;
+    double price;
+    string brand;
+    float weight;
+    public:
+    virtual void readfile(istream &is)=0;
+    virtual void writefile(ostream &os)=0;
+    virtual void inputfile(istream &is)=0;
+    virtual double price_discount(int qty) const = 0;
+    virtual string type() const = 0;
+    string getname() { return name; };
+    string getid() { return id; };
+    double getprice() { return price; };
+};
+class FoodProduct:product {
+    string expiry;
+    string nutrition;
+    public:
+    void readfile(istream &is);
+    void writefile(ostream &os);
+    void inputfile(istream &is);
+    double price_discount(int qty) const;
+    string type() const;
+
+};
+class ElectronicProduct:product {
+    string warranty;
+    string seri;
 
 
-}
+    public:
+    void readfile(istream &is);
+    void writefile(ostream &os);
+    void inputfile(istream &is);
+    double price_discount(int qty) const;
+    string type() const;
+    void quality();
+
+};
+class Householdproduct:product {
+    string material;
+    string usage;
+    public:
+    void readfile(istream &is);
+    void writefile(ostream &os);
+    void inputfile(istream &is);
+    double price_discount(int qty) const;
+    string type() const;
+
+};
+class Supermarket {
+public:
+
+    std::vector<std::unique_ptr<product>> danhSachSanPham;
+
+    Supermarket() = default;
+    ~Supermarket() = default;
 
 
+    bool taiTuFile(const std::string& tenFile);
+    bool luuVaoFile(const std::string& tenFile) const;
+
+
+    bool them_sp(std::unique_ptr<product> sp);
+    bool xao_sp(const std::string& maSanPham);
+
+
+    product* timTheoMa(const std::string& maSanPham) const;
+
+
+    bool capNhatSoLuong(const std::string& maSanPham, int slMoi);
+    bool tangSoLuong(const std::string& maSanPham, int sl);
+    bool giamSoLuong(const std::string& maSanPham, int sl);
+
+
+    std::vector<product*> layTatCa() const;
+
+};
+
+
+class Cart {
+public:
+
+    struct MucGio {
+        std::string maSanPham;
+        product* sanPhamPtr;
+        int soLuong;
+        double donGiaGoc;
+
+        MucGio() : sanPhamPtr(nullptr), soLuong(0), donGiaGoc(0.0) {}
+        MucGio(product* sp, int sl)
+            : sanPhamPtr(sp), soLuong(sl), donGiaGoc(sp ? sp->getprice() : 0.0) {
+            if (sp) maSanPham = sp->getid();
+        }
+
+    };
+    std::vector<MucGio> danhSach;
+    bool them(product*p, int so_luong);
+    bool xoa(const string& id);
+
+    bool thay_sl(const string&id, int so_luong_moi);
+    double tong_tien();
+
+
+};
+class Customer {
+public:
+    std::string maKhach;
+    std::string tenKhach;
+    Cart gioHang;
+
+    Customer() = default;
+    Customer(std::string ma, std::string ten)
+        : maKhach(std::move(ma)), tenKhach(std::move(ten)) {}
+
+    void xoaGioHang() { gioHang.danhSach.clear(); }
+
+
+    bool themSanPhamVaoGioHang(Supermarket& sieuThi, const std::string& maSanPham, int soLuong);
+    bool xoaSanPhamKhoiGioHang(const std::string& maSanPham);
+    bool thayDoiSoLuongTrongGioHang(const std::string& maSanPham, int soLuongMoi);
+
+};
+class OrderItem {
+public:
+    std::string maSanPham;
+    std::string ten;
+    int soLuong;
+    double donGiaGoc;
+    double donGiaSauGiam;
+    double thanhTien;
+
+    OrderItem() : soLuong(0), donGiaGoc(0), donGiaSauGiam(0), thanhTien(0) {}
+};
+class Order {
+public:
+    std::string order_id;
+    std::string customer_id;
+    std::string date;
+    std::vector<OrderItem> danhSachMuc;
+
+    Order() = default;
+    Order(std::string maDon_, std::string maKhach_, std::string ngayGio_)
+        : order_id(maDon_), customer_id(maKhach_), date(ngayGio_) {}
+
+    double tongTien() const;
+    std::string toTextHoaDon() const;
+    bool ghiRaFile(const std::string& tenFile) const;
+
+    static std::optional<Order>
+    taoTuCartVaCheckout(const Customer& khach,
+                        Supermarket& sieuThi,
+                        const std::string& maDonMoi);
+};
