cf6aec91077feaf1249661bd25166039aa738916
DucThinhmta <nguyenducthinh28092006@gmail.com>
2025-12-09 22:50:01 +0700

    Add ElectronicProduct class with attributes and methods

--- HouseholdProduct.h
+++ HouseholdProduct.h
@@ -1,1 +1,20 @@
+#include"product.h"
+#include<vector>
+class ElectronicProduct:product{
+    string material;
+    string usage;
+public:
+    void setId(const string &v){id =v; };
+    void setName(const std::string& v) { name = v; }
+    void setPrice(double v) { price = v; }
+    void setQuantity(int v) { quantity = v; }
+    void setBrand(const std::string& v) { brand = v; }
+    void setWeight(float v) { weight = v; }
+    void setMaterial(const std::string& v) { Material= v; }
+    void setUsage(const std::string& v) { Usage= v; }
+    string getMaterial() const { return material; }
+    string getUsage() const { return usage; }
+    double price_discount(int qty) const;
+    string type() const;
+};
 
