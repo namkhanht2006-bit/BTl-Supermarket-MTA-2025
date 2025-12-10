22bb63bf1ec7edd910690e03c871b59047309e8f
DucThinhmta <nguyenducthinh28092006@gmail.com>
2025-12-10 09:22:56 +0700

    Update HouseholdProduct.cpp

--- HouseholdProduct.cpp
+++ HouseholdProduct.cpp
@@ -1,6 +1,20 @@
 include <iostream>
 #include "HouseholdProduct.h"
 using namespace std;
+Householdproduct::Householdproduct(string Usage, string Material) {
+	usage = Usage; material = Material;
+}
+Householdproduct::Householdproduct(){}
+Householdproduct::~Householdproduct(){}
+
+ostream& operator<<(ostream& out, const Householdproduct& p) {
+    p.print(out);
+    return out;
+}
+istream& operator>>(istream& in, Householdproduct& p) {
+    p.input(in);
+    return in;
+}
 string HouseholdProduct::type() const {
     return "Household";
 }
@@ -11,4 +25,13 @@ double HouseholdProduct::price_discount(int qty) const {
     if (p.price>=200&&p.price <500) return price * qty * 0.90;
     return price * qty;
 }
-
+void Householdproduct::them(istream& in) {
+    product::them(in);
+    cout << "Enter Usage: "; getline(in, usage);
+    cout << "Enter Material: "; getline(in,material;
+}
+void Householdproduct::in(ostream& out) const {
+    product::in(out);
+    out << "Usage: " << usage << endl;
+    out << "Material: " << material<< endl;
+}
