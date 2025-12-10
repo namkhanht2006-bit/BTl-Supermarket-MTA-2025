fc03c7cb175c43e5a8af40a22735b9d01fc5a6e7
DucThinhmta <nguyenducthinh28092006@gmail.com>
2025-12-10 08:30:37 +0700

    Update product.cpp

--- product.cpp
+++ product.cpp
@@ -1,18 +1,12 @@
 #include"product.h"
 using namespace std;
-void product::them() {
-    cout<<"Nhap ma san pham"<<endl;
-    cin>>id;
-    cout<<"Nhap ten san pham"<<endl;
-    cin>>name;
-    cout<<"Nhap gia san pham"<<endl;
-    cin>>price;
-    cout<<"Nhap thuong hieu san pham"<<endl;
-    cin>>brand;
-    cout<<"Nhap so luong san pham"<<endl;
-    cin>>quantity;
-    cout<<"Nhap khoi luong san pham"<<endl;
-    cin>>weight;
+void product::them(istream &in) {
+    cout<<"Nhap ma san pham";getline(in,id);
+    cout<<"Nhap ten san pham";getline(in,name);
+    cout<<"Nhap gia san pham";in>>price;in.ignore();
+    cout<<"Nhap thuong hieu san pham";getline(in,brand);
+    cout<<"Nhap so luong san pham";getline(in,quantity);
+    cout<<"Nhap khoi luong san pham";getline(in,weight);
 }
 void product::in() {
     cout<<"Ma so: "<<id<<endl;
@@ -22,3 +16,13 @@ void product::in() {
     cout<<"So luong san pham: "<<quantity<<endl;
     cout<<"Can nang: "<<weight<<endl;
 }
+ostream& operator<<(ostream& out, const product& p) {
+    p.print(out);
+    return out;
+}
+
+istream& operator>>(istream& in, product& p) {
+    p.input(in);
+    return in;
+}
+
