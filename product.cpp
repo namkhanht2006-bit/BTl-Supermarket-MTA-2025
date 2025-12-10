bba128339ed7805c7a56c683474674abf8cb1679
namkhanht2006-bit <namkhanht2006@gmail.com>
2025-12-10 09:06:12 +0700

    Update product.cpp

--- product.cpp
+++ product.cpp
@@ -5,52 +5,22 @@ void product::them(istream &in) {
     cout<<"Nhap ten san pham";getline(in,name);
     cout<<"Nhap gia san pham";in>>price;in.ignore();
     cout<<"Nhap thuong hieu san pham";getline(in,brand);
-    cout<<"Nhap so luong san pham";getline(in,quantity);
-    cout<<"Nhap khoi luong san pham";getline(in,weight);
-}
-void product::in() {
-    cout<<"Ma so: "<<id<<endl;
-    cout<<"Ten: "<<name<<endl;
-    cout<<"Price: "<<price<<endl;
-    cout<<"Thuong hieu: "<<brand<<endl;
-    cout<<"So luong san pham: "<<quantity<<endl;
-    cout<<"Can nang: "<<weight<<endl;
-}
-ostream& operator<<(ostream& out, const product& p) {
-    p.print(out);
-    return out;
+    cout<<"Nhap so luong san pham";in>>quantity;
+    cout<<"Nhap khoi luong san pham";in>>weight;
 }
 
-istream& operator>>(istream& in, product& p) {
-    p.input(in);
-    return in;
+void product::in(ostream &os) {
+    cout<<"Ma so: ";os<<id;
+    cout<<"Ten: ",os<<name<<endl;
+    cout<<"Price: ", os<<price<<endl;
+    cout<<"Thuong hieu: ",os<<brand<<endl;
+    cout<<"So luong san pham: ",os<<quantity<<endl;
+    cout<<"Can nang: ",os<<weight<<endl;
 }
-//fix
-#include"product.h"
-using namespace std;
-void product::them(istream& in) {
-    cout << "Nhap ma san pham";getline(in, id);
-    cout << "Nhap ten san pham";getline(in, name);
-    cout << "Nhap gia san pham";in >> price;in.ignore();
-    cout << "Nhap thuong hieu san pham";getline(in, brand);
-    cout << "Nhap so luong san pham";in >> quantity;in.ignore();
-    cout << "Nhap khoi luong san pham";in >> weight;in.ignore();
-}
-void product::in(ostream& out) {
-    cout << "Ma so: " << id << endl;
-    cout << "Ten: " << name << endl;
-    cout << "Price: " << price << endl;
-    cout << "Thuong hieu: " << brand << endl;
-    cout << "So luong san pham: " << quantity << endl;
-    cout << "Can nang: " << weight << endl;
-}
-ostream& operator<<(ostream& out, product& p) {
-    p.in(out);
-    return out;
+istream &operator>>(istream &is, product &p) {
+    p.them(is);
 }
 
-istream& operator>>(istream& in, product& p) {
-    p.them(in);
-    return in;
+ostream &operator<<(ostream &os, product &p) {
+    p.in(os);
 }
-
