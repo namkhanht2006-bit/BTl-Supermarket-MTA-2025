ef277fe87a355a41e48083cdfc03f095126fca2f
DucThinhmta <nguyenducthinh28092006@gmail.com>
2025-12-10 13:57:51 +0700

    Update Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -1,3 +1,28 @@
-
-
 #include "Customer.h"
+Customer::Customer(string name, string gender,string phonenumber, string address, double age) {
+	Name = name; Gender = gender; PhoneNumber = phonenumber; Address = address; Age = age;
+}
+Customer::Customer() {
+	Name = "Thinh dz"; Gender = ' '; PhoneNumber = "0378593671"; Address = "Ha Noi"; Age = 19;
+}
+Customer::~Customer(){}
+void Customer::input() {
+	cout << "nhap ten khach hang:"; getline(cin, Name);
+	cout << "nhap tuoi khach hang:"; cin >> Age; cin.ignore();
+	cout << "nhap gioi tinh khach hang:"; getline(cin,Gender);
+	cout << "nhap sdt khach hang:"; cin >> PhoneNumber; cin.ignore();
+	cout << "nhap dia chi khach hang:"; getline(cin, Address);
+}
+void Customer::output() {
+	cout << "Ho va ten : " << Name;
+	cout << "Tuoi:" << Age;
+	cout << "Gioi tinh:" << Gender;
+	cout << "Sdt:" << PhoneNumber;
+	cout << "Dia chi" << Address;
+}
+void Customer::Addcart(product* p) {
+	cart.AddProduct(p);//phuong thuc them san pham cua class cart
+}
+void Customer::Deletecart(const string& id) {
+	cart.RemoveByID(id);//phuong thuc xoa san pham cua class cart
+}
