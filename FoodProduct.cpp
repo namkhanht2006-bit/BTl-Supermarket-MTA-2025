d1c44b7d91f717b4f7c31f9fb4b30170c2fe64ce
DucThinhmta <nguyenducthinh28092006@gmail.com>
2025-12-10 09:14:26 +0700

    Implement Foodproduct class with constructors and input method
    Added constructors, destructor, and input method for Foodproduct class.

--- FoodProduct.cpp
+++ FoodProduct.cpp
@@ -1,6 +1,11 @@
 include <iostream>
 #include "FoodProduct.h"
 using namespace std;
+Foodproduct::Foodproduct(string expiry,  string nutrition) :product::product(){
+	Expiry = expiry; Nutrition = nutrition;
+}
+Foodproduct::Foodproduct(){}
+Foodproduct::~Foodproduct(){}
 string FoodProduct::type() const {
     return "Food";
 }
@@ -10,6 +15,13 @@ double FoodProduct::price_discount(int qty) const {
     if (p.price>=500) return price * qty*0.9;
     if (p.price>=300&&p.price <500) return price * qty * 0.94;
     return price * qty;
+}
+void Foodproduct::them(istream& in) {
+    product::them(in);
+    cout << "Enter Expiry: "; getline(in, Expiry);
+    cout << "Enter Allergy: "; getline(in, Allergy);
+    cout << "Enter Nutrition: "; getline(in, Nutrition);
+    cout << "Enter Storage: "; getline(in, Storage);
 }
 
 
