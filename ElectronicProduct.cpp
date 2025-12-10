89cd4aef209d93b66fce5ea6d3b8609c8233e60b
namkhanht2006-bit <namkhanht2006@gmail.com>
2025-12-10 08:47:17 +0700

    Update ElectronicProduct.cpp

--- ElectronicProduct.cpp
+++ ElectronicProduct.cpp
@@ -10,5 +10,26 @@ double ElectronicProduct::price_discount(int qty) const {
     if (qty >= 10) return price * qty * 0.95;
     return price * qty;
 }
+void ElectronicProduct::them() {
+    product::them();
+    cout<<"Nhap bao hanh: "<<endl;
+    cin>>warranty;
+    cout<<"Nhap so seri: "<<endl;
+    cin>>seri;
 
+}
+void ElectronicProduct::in() {
+    product::in();
+    cout<<"Thoi han bao hanh: "<<warranty<< endl;
+    cout<<"So seri: "<<seri<<endl;
+}
+
+istream& operator>>(istream& is, ElectronicProduct& e) {
+    e.them();
+}
+
+ostream& operator<<(ostream& os, ElectronicProduct& e) {
+    e.in();
+
+}
 
