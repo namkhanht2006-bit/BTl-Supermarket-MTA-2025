25462235c8e02d1a520a4bbfd0eef5f320199348
namkhanht2006-bit <namkhanht2006@gmail.com>
2025-12-10 09:12:13 +0700

    Update ElectronicProduct.h

--- ElectronicProduct.h
+++ ElectronicProduct.h
@@ -9,9 +9,8 @@ public:
     string getSeri() const { return seri; }
     double price_discount(int qty) const;
     string type() const;
-    void them() override;
-    void in() override;
+    void them(istream &is) override;
+    void in(ostream &os) override;
     friend istream& operator>>(istream& is, ElectronicProduct& e);
     friend ostream& operator<<(ostream& os, ElectronicProduct& e);
 };
-
