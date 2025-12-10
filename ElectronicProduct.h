0e8a9ad3ca8ee5c4cd863bf1d54d8e3a2b919697
namkhanht2006-bit <namkhanht2006@gmail.com>
2025-12-10 08:19:35 +0700

    Update ElectronicProduct.h

--- ElectronicProduct.h
+++ ElectronicProduct.h
@@ -4,20 +4,12 @@ class ElectronicProduct:product{
     string warranty;
     string seri;
 public:
-    void setId(const string &v){id =v; };
-    void setName(const std::string& v) { name = v; }
-    void setPrice(double v) { price = v; }
-    void setQuantity(int v) { quantity = v; }
-    void setBrand(const std::string& v) { brand = v; }
-    void setWeight(float v) { weight = v; }
-
-    void setWarranty(const std::string& v) { warranty = v; }
-    void setSeri(const std::string& v) { seri = v; }
-
-    std::string getWarranty() const { return warranty; }
-    std::string getSeri() const { return seri; }
+    string getWarranty() const { return warranty; }
+    string getSeri() const { return seri; }
     double price_discount(int qty) const;
     string type() const;
-
+    void nhap();
+    void in()
 };
+
 
