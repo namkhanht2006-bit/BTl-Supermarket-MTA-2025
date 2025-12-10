e821f38a3e8deb8e2ec56442ba8bcda553ac21f1
namkhanht2006-bit <namkhanht2006@gmail.com>
2025-12-10 08:17:24 +0700

    Update product.cpp

--- product.cpp
+++ product.cpp
@@ -1,2 +1,24 @@
-
-#include "product.h"
+#include"product.h"
+using namespace std;
+void product::them() {
+    cout<<"Nhap ma san pham"<<endl;
+    cin>>id;
+    cout<<"Nhap ten san pham"<<endl;
+    cin>>name;
+    cout<<"Nhap gia san pham"<<endl;
+    cin>>price;
+    cout<<"Nhap thuong hieu san pham"<<endl;
+    cin>>brand;
+    cout<<"Nhap so luong san pham"<<endl;
+    cin>>quantity;
+    cout<<"Nhap khoi luong san pham"<<endl;
+    cin>>weight;
+}
+void product::in() {
+    cout<<"Ma so: "<<id<<endl;
+    cout<<"Ten: "<<name<<endl;
+    cout<<"Price: "<<price<<endl;
+    cout<<"Thuong hieu: "<<brand<<endl;
+    cout<<"So luong san pham: "<<quantity<<endl;
+    cout<<"Can nang: "<<weight<<endl;
+}
