b010242cac659f043fe8dc36310ed6fa9d007577
DucThinhmta <nguyenducthinh28092006@gmail.com>
2025-12-10 08:26:36 +0700

    Update product.h

--- product.h
+++ product.h
@@ -1,6 +1,7 @@
 #include<string>
 #include<iostream>
 #include<vector>
+#include <fstream>
 using namespace std;
 class product {
 protected:
@@ -13,10 +14,12 @@ protected:
     public:
     virtual double price_discount(int qty) const = 0;
     virtual string type() const = 0;
-
-    virtual void them();
-    virtual void in();
+    virtual void them(istream& in);
+    virtual void in(ostream &out);
     string getname() { return name; };
     string getid() { return id; };
     double getprice() { return price; };
+    friend ostream& operator<<(ostream& out, const product& p);
+    friend istream& operator>>(istream& in, product& p);
 };
+
