d1a7be5e5f89a4d1af230cffc018447c9ac9e6f0
baoga180726-it <baoga180726@gmail.com>
2025-12-11 15:05:43 +0700

    Refactor Supermarket class to use smart pointers

--- Supermarket.h
+++ Supermarket.h
@@ -3,23 +3,31 @@
 #include <memory>
 #include <string>
 #include "product.h"
-#include "food.h"
-#include "electronic.h"
-#include "household.h"
-using namespace std;
+#include "FoodProduct.h"
+#include "ElectronicProduct.h"
+#include "HouseholdProduct.h"
+
 class Supermarket {
+private:
+
+    std::vector<std::unique_ptr<product>> danhSachSanPham;
+
 public:
-
-    std::vector<Product*> danhSachSanPham;
-
     Supermarket() = default;
+
+    Supermarket(const Supermarket&) = delete;
+    Supermarket& operator=(const Supermarket&) = delete;
+
+    Supermarket(Supermarket&&) = default;
+    Supermarket& operator=(Supermarket&&) = default;
+
     ~Supermarket() = default;
 
     bool taiTuFile(const std::string& tenFile);
     bool luuVaoFile(const std::string& tenFile) const;
 
     bool them_sp(std::unique_ptr<product> sp);
-    bool xao_sp(const std::string& maSanPham);
+    bool xoa_sp(const std::string& maSanPham);
 
     product* timTheoMa(const std::string& maSanPham) const;
 
@@ -27,5 +35,8 @@ public:
     bool tangSoLuong(const std::string& maSanPham, int sl);
     bool giamSoLuong(const std::string& maSanPham, int sl);
 
-    vector<product*> layTatCa() const;
+    const std::vector<std::unique_ptr<product>>& layTatCa() const {
+        return danhSachSanPham;
+    }
 };
+
