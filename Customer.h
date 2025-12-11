dc09f724281dad734223bb2a5203c74d506eba20
DucThinhmta <nguyenducthinh28092006@gmail.com>
2025-12-11 09:18:31 +0700

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -1,26 +1,20 @@
-#pragma once
-#include <string>
-#include "Supermarket.h"
 #include "cart.h"
 using namespace std;
 class Customer
 {private :
+	Cart cart;
 	string Name;
 	string Gender;
 	string PhoneNumber;
 	string Address;
 	double Age;
 public:
-	Cart cart;
+	
 	Customer();
 	Customer(string name, string gender, string phonenumber, string address, double age);
 	~Customer();
 	void input();
 	void output();
-	void Buy(Supermarket &sp);
-	void Deletecart();
-	void Changecart();
-	//void Printcart();
-	//void Bill(const string& filename);
+	Cart& getCart() { return cart; }
+	const Cart& getCart() const { return cart;} 
 };
-
