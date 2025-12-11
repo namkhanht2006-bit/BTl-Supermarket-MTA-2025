5f0b444a52c37c347acce4e9a9c27fad430f82a1
baoga180726-it <baoga180726@gmail.com>
2025-12-11 15:26:12 +0700

    Refactor product input/output methods for clarity

--- product.cpp
+++ product.cpp
@@ -1,31 +1,44 @@
 #include"product.h"
 using namespace std;
 void product::them(istream &in) {
-    cout<<"Nhap ma san pham";getline(in,id);
-    cout<<"Nhap ten san pham";getline(in,name);
-    cout<<"Nhap gia san pham";in>>price;in.ignore();
-    cout<<"Nhap thuong hieu san pham";getline(in,brand);
-    cout<<"Nhap so luong san pham";in>>quantity;
-    cout<<"Nhap khoi luong san pham";in>>weight;
+    cout << "Nhap ma san pham: ";
+    getline(in, id);
+
+    cout << "Nhap ten san pham: ";
+    getline(in, name);
+
+    cout << "Nhap gia san pham: ";
+    in >> price;
+
+    cout << "Nhap thuong hieu san pham: ";
+    in.ignore();  
+    getline(in, brand);
+
+    cout << "Nhap so luong san pham: ";
+    in >> quantity;
+
+    cout << "Nhap khoi luong san pham: ";
+    in >> weight;
 }
 
 void product::in(ostream &os) {
-    cout<<"Ma so: ",os<<id;
-    cout<<"Ten: ",os<<name<<endl;
-    cout<<"Price: ", os<<price<<endl;
-    cout<<"Thuong hieu: ",os<<brand<<endl;
-    cout<<"So luong san pham: ",os<<quantity<<endl;
-    cout<<"Can nang: ",os<<weight<<endl;
+    os << "Ma so: " << id << endl;
+    os << "Ten: " << name << endl;
+    os << "Gia: " << price << endl;
+    os << "Thuong hieu: " << brand << endl;
+    os << "So luong: " << quantity << endl;
+    os << "Khoi luong: " << weight << endl;
 }
+
 istream &operator>>(istream &is, product &p) {
     p.them(is);
+    return is;
 }
 
 ostream &operator<<(ostream &os, product &p) {
     p.in(os);
+    return os;
 }
-bool Can_sell(int qty)  {
-        return qty > 0 && quantity >= qty;
-    }
+
 
 
