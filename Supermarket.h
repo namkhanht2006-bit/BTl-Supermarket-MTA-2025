920bb1141294a038247d6f4a033690d071cf0b96
baoga180726-it <baoga180726@gmail.com>
2025-12-12 14:28:55 +0700

    Rename function xao_sp to xoa_sp

--- Supermarket.h
+++ Supermarket.h
@@ -19,7 +19,7 @@ public:
     bool luuVaoFile(const std::string& tenFile) const;
 
     bool them_sp(std::unique_ptr<product> sp);
-    bool xao_sp(const std::string& maSanPham);
+    bool xoa_sp(const std::string& maSanPham);
 
     product* timTheoMa(const std::string& maSanPham) const;
 
