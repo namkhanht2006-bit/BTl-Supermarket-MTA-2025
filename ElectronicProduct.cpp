e455970093896f961ccc05860253427f0f6f56be
DucThinhmta <nguyenducthinh28092006@gmail.com>
2026-01-09 20:25:25 +0700

    Update ElectronicProduct.cpp

--- ElectronicProduct.cpp
+++ ElectronicProduct.cpp
@@ -6,21 +6,20 @@ string ElectronicProduct::type() const {
 }
 
 double ElectronicProduct::price_discount(int qty) const {
-    if (qty >= 50) return price * qty * 0.9;
-    if (qty >= 10) return price * qty * 0.95;
+    if (price * qty >= 500000) return price * qty * 0.9;
+    else if (price * qty < 500000 && price * qty >= 300000) return price * qty * 0.95;
     return price * qty;
 }
-void ElectronicProduct::them(istream &is) {
+
+void ElectronicProduct::them(istream& is) {
     product::them(is);
-    cout<<"Nhap bao hanh: "<<endl;
-    is>>warranty;
-
-
+    cout << "Nhap bao hanh: " << endl;
+    is >> warranty;
 }
-void ElectronicProduct::in(ostream &os) const {
-    product::in(os);
-    os<<"Thoi han bao hanh: "<<warranty<< endl;
 
+void ElectronicProduct::in(ostream& os) const {
+    product::in(os);
+    os << "Thoi han bao hanh: " << warranty << endl;
 }
 
 istream& operator>>(istream& is, ElectronicProduct& e) {
@@ -36,12 +35,19 @@ ostream& operator<<(ostream& os, ElectronicProduct& e) {
 
 
 bool ElectronicProduct::Can_sell(int qty) const { return product::Can_sell(qty); }
-ElectronicProduct::ElectronicProduct() {
-     warranty="";
 
-}
 
-void ElectronicProduct::them_file(istream &is) {
+void ElectronicProduct::them_file(istream& is) {
     product::them_file(is);
-    is>>warranty;
+    is >> warranty;
 }
+
+ElectronicProduct::ElectronicProduct() {
+    warranty = 12/12/2006;
+}
+
+ElectronicProduct::ElectronicProduct(string warranty) {
+    this->warranty = warranty;
+}
+
+ElectronicProduct::~ElectronicProduct(){}
