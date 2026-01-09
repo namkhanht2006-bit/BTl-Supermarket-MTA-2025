a9d73e590384cc79355cf0d817d98cd4ed18220a
DucThinhmta <nguyenducthinh28092006@gmail.com>
2026-01-09 20:27:38 +0700

    Update HouseholdProduct.h

--- HouseholdProduct.h
+++ HouseholdProduct.h
@@ -1,25 +1,25 @@
+
 #include"product.h"
 #include<vector>
 class HouseholdProduct : public product {
     string material;
     string usage;
 public:
-    void them(istream& in) ;
-    void in(ostream& out) const ;
-    string getMaterial() const { return material; }
-    string getUsage() const { return usage; }
+    void them(istream& in);
+    void in(ostream& out) const;
+    void them_file(istream& is);
     double price_discount(int qty) const;
     string type() const;
     bool Can_sell(int qty) const;
-    HouseholdProduct();
-    HouseholdProduct(string Usage,string Material);
-    ~HouseholdProduct();
     friend  ostream& operator<<(ostream& out, const HouseholdProduct& p);
     friend  istream& operator>>(istream& in, HouseholdProduct& p);
-
+    
+    HouseholdProduct();
+    HouseholdProduct(string Usage, string Material);
+    ~HouseholdProduct();
+    
     void setMaterial(std::string mtr) { material = mtr; }
     void setUsage(string usg) { usage = usg; }
-
-    void them_file(istream &is);
+    string getMaterial() const { return material; }
+    string getUsage() const { return usage; }
 };
-
