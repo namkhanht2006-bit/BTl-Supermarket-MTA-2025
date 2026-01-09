3a7816d696b070e50df5f29a52d0e000ec667e87
DucThinhmta <nguyenducthinh28092006@gmail.com>
2026-01-09 20:25:46 +0700

    Update FoodProduct.cpp

--- FoodProduct.cpp
+++ FoodProduct.cpp
@@ -5,31 +5,30 @@ using namespace std;
 FoodProduct::FoodProduct(string expiry_, string nutrition_) {
     expiry = expiry_; nutrition = nutrition_;
 }
-FoodProduct::FoodProduct(){}
-FoodProduct::~FoodProduct(){}
+FoodProduct::FoodProduct() {}
+FoodProduct::~FoodProduct() {}
 
 string FoodProduct::type() const {
     return "Food";
 }
 
 double FoodProduct::price_discount(int qty) const {
-    if (price >= 500) return price * qty * 0.9;
-    if (price >= 300 && price < 500) return price * qty * 0.94;
+    if (price*qty>=300000) return price * qty * 0.8;
+    if (price*qty< 300 && price*qty >100000) return price * qty * 0.9;
     return price * qty;
 }
 
 void FoodProduct::them(istream& in) {
     product::them(in);
-    // read expiry and nutrition
     in.ignore();
-    cout << "Enter expiry: "; getline(in, expiry);
-    cout << "Enter nutrition: "; getline(in, nutrition);
+    cout << "Nhap han su dung: "; getline(in, expiry);
+    cout << "Nhap gia tri dinh duong: "; getline(in, nutrition);
 }
 
 void FoodProduct::in(ostream& out) const {
     product::in(out);
-    out << "Expiry: " << expiry << endl;
-    out << "Nutrition: " << nutrition << endl;
+    out << "Han su dung: " << expiry << endl;
+    out << "Dinh duong: " << nutrition << endl;
 }
 
 ostream& operator<<(ostream& out, const FoodProduct& p) {
@@ -44,5 +43,5 @@ istream& operator>>(istream& in, FoodProduct& p) {
 bool FoodProduct::Can_sell(int qty) const { return product::Can_sell(qty); }
 void FoodProduct::them_file(istream& in) {
     product::them_file(in);
-    in>>expiry>>nutrition;;
+    in >> expiry >> nutrition;;
 }
