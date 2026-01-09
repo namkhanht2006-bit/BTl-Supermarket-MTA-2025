09e774288c76328df3b7298d0fae1de22cc88828
DucThinhmta <nguyenducthinh28092006@gmail.com>
2026-01-09 20:26:33 +0700

    Update product.h

--- product.h
+++ product.h
@@ -13,15 +13,12 @@ protected:
     string brand;
     float weight;
     int quantity;
-    public:
+public:
     virtual double price_discount(int qty) const = 0;
     virtual string type() const = 0;
     virtual void them(istream& in);
     virtual void them_file(istream& is);
-    virtual void in(ostream &out) const;
-    string getname() { return name; };
-    string getid() { return id; };
-    double getprice() { return price; };
+    virtual void in(ostream& out) const;
     friend ostream& operator<<(ostream& out, const product& p);
     friend istream& operator>>(istream& in, product& p);
     virtual bool Can_sell(int qty) const;
@@ -32,15 +29,9 @@ protected:
     void setbrand(const std::string& v) { brand = v; }
     void setquantity(int v) { quantity = v; }
     void setweight(double v) { weight = v; }
-
     std::string getid() const { return id; }
     std::string getname() const { return name; }
     double getprice() const { return price; }
     int getquantity() const { return quantity; }
     double getweight() const { return weight; }
-
-
 };
-
-
-
