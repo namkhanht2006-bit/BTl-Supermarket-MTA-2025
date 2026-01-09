ac57c8ff53cff9a0a67dad921219a8a1392e5600
DucThinhmta <nguyenducthinh28092006@gmail.com>
2026-01-09 20:27:02 +0700

    Update FoodProduct.h

--- FoodProduct.h
+++ FoodProduct.h
@@ -2,29 +2,28 @@
 #include "product.h"
 #include <string>
 #include <iostream>
-
 class FoodProduct : public product {
     std::string expiry;
     std::string nutrition;
 public:
-    void them(std::istream& in) ;
-    void in(std::ostream &out) const ;
-    std::string getExpiry() const { return expiry; }
-    std::string getNutrition() const { return nutrition; }
-    double price_discount(int qty) const ;
-    std::string type() const ;
-    bool Can_sell(int qty) const ;
+    void them(std::istream& in);
+    void in(std::ostream& out) const;
+    double price_discount(int qty) const;
+    std::string type() const;
+    bool Can_sell(int qty) const;
+    void them_file(istream& in); 
+    friend std::ostream& operator<<(std::ostream& out, const FoodProduct& p);
+    friend std::istream& operator>>(std::istream& in, FoodProduct& p);
+    
     FoodProduct();
     FoodProduct(std::string expiry, std::string nutrition);
     ~FoodProduct();
-    friend std::ostream& operator<<(std::ostream& out, const FoodProduct& p);
-    friend std::istream& operator>>(std::istream& in, FoodProduct& p);
+  
 
     void setExpiry(std::string exp) { expiry = exp; }
     void setNutrition(std::string exp) { nutrition = exp; }
-
     string getexpiry() { return expiry; };
     string getNutrition() { return nutrition; };
 
-    void them_file(istream& in) ;
+    
 };
